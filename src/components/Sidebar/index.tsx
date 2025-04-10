ef84d2b9115f5b8e48606cd8b8c793ac71378ef4
Beatriz Cavallieri <bfcavallieri@gmail.com>
2025-04-10 10:55:32 -0300

    style: Input styling

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,16 @@
+import { Search } from "lucide-react"
 import { Logo } from "./Logo"
 
 const Sidebar = () => {
   return (
-    <aside className="border-r border-zinc-200 py-8 px-6">
+    <aside className="border-r border-zinc-200 py-8 px-6 space-y-6">
       <Logo />
+      <div className="flex w-full items-center gap-2 border border-zinc-300 rounded-lg py-2.5 px-3.5 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          className="border-0 p-0 bg-transparent text-zinc-900 placeholder-zinc-500 flex-1"
+          placeholder="Search" />
+      </div>
     </aside>
   )
 }
