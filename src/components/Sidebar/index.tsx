7475dc50448b26b6d174286de28ff920e1a13b26
Beatriz Cavallieri <bfcavallieri@gmail.com>
2025-04-16 22:15:26 -0300

    feat: creates Input component following the React composition pattern

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,17 +3,20 @@ import { Logo } from "./Logo"
 import Navigation from "../Navigation"
 import UsedSpaceWidget from "./UsedSpaceWidget"
 import UserProfile from "./UserProfile"
+import * as Input from '../Input'
 
 const Sidebar = () => {
   return (
     <aside className="border-r border-zinc-200 py-8 px-6 flex flex-col gap-6">
       <Logo />
-      <div className="flex w-full items-center gap-2 border border-zinc-300 rounded-lg py-2.5 px-3.5 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="border-0 p-0 bg-transparent text-zinc-900 placeholder-zinc-500 flex-1"
-          placeholder="Search" />
-      </div>
+
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+      
       <Navigation />
       <UsedSpaceWidget />
       <div className="h-px bg-zinc-200" />
