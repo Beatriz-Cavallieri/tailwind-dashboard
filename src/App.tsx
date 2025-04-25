432a26c58b8e5d2aa7297eef038749d4d4455c5b
Beatriz Cavallieri <bfcavallieri@gmail.com>
2025-04-24 22:24:26 -0300

    moves non form elements outside of form component

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,33 @@ const App = () => {
             <Sidebar />
             <main className="px-6 py-8">
                 <h1 className="text-3xl text-zinc-900 font-medium">
-                Settings
+                    Settings
                 </h1>
                 <SettingsTabs></SettingsTabs>
-                <PersonalInfoForm/>
+
+                <div className="mt-6 flex flex-col">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+                            <span className="text-sm text-zinc-500">
+                                Update your photo and personal details here.
+                            </span>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <button
+                                type="button"
+                                className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50 border border-zinc-300"
+                            >Cancel</button>
+                            <button
+                                type="submit"
+                                form="settings"
+                                className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-white bg-violet-600 hover:bg-violet-700"
+                            >Save</button>
+                        </div>
+                    </div>
+
+                    <PersonalInfoForm />
+                </div>
             </main>
 
         </div>
--- src/components/PersonalInfoForm/index.tsx
+++ src/components/PersonalInfoForm/index.tsx
@@ -2,139 +2,119 @@ import * as Input from "../Input"
 
 const PersonalInfoForm = () => {
   return (
-    <div className="mt-6 flex flex-col">
-      <div className="flex justify-between items-center">
-        <div>
-          <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
-          <span className="text-sm text-zinc-500">
-            Update your photo and personal details here.
-          </span>
-        </div>
-        <div className="flex gap-2 items-center">
-          <button
-            type="button"
-            className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50 border border-zinc-300"
-          >Cancel</button>
-          <button
-            type="submit"
-            form="settings"
-            className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-white bg-violet-600 hover:bg-violet-700"
-          >Save</button>
+    <form
+      id="settings"
+      className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200">
+      <div className="grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="firstName"
+          className="text-sm text-zinc-700 font-medium">
+          Name
+        </label>
+        <div className="grid grid-cols-2 gap-6">
+          <Input.Root>
+            <Input.Control id="firstName" defaultValue="Olivia" />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Control defaultValue="Rhye" />
+          </Input.Root>
         </div>
       </div>
 
-      <form id="settings" className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200">
-        <div className="grid grid-cols-3">
-          <label
-            htmlFor="firstName"
-            className="text-sm text-zinc-700 font-medium">
-            Name
-          </label>
-          <div className="flex gap-2">
-            <Input.Root>
-              <Input.Control id="firstName" defaultValue="Olivia" />
-            </Input.Root>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="email"
+          className="text-sm text-zinc-700 font-medium">
+          Email address
+        </label>
+        <Input.Root>
+          <Input.Control
+            id="email"
+            type="email"
+            defaultValue="olivia@untitledui.com"
+          />
+        </Input.Root>
+      </div>
 
-            <Input.Root>
-              <Input.Control defaultValue="Rhye" />
-            </Input.Root>
-          </div>
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="email"
+          className="text-sm text-zinc-700 font-medium">
+          Your photo
+          <span >
+            This will be displayed on your profile.
+          </span>
+        </label>
+        <div />
+      </div>
 
-        <div>
-          <label
-            htmlFor="email"
-            className="text-sm text-zinc-700 font-medium">
-            Email address
-          </label>
-          <Input.Root>
-            <Input.Control
-              id="email"
-              type="email"
-              defaultValue="olivia@untitledui.com"
-            />
-          </Input.Root>
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="role"
+          className="text-sm text-zinc-700 font-medium">
+          Role
+        </label>
+        <Input.Root>
+          <Input.Control id="role" defaultValue="Product Designer" />
+        </Input.Root>
+      </div>
 
-        <div>
-          <label
-            htmlFor="email"
-            className="text-sm text-zinc-700 font-medium">
-            Your photo
-            <span >
-              This will be displayed on your profile.
-            </span>
-          </label>
-          <div />
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="country"
+          className="text-sm text-zinc-700 font-medium">
+          Country
+        </label>
+        <div />
+      </div>
 
-        <div >
-          <label
-            htmlFor="role"
-            className="text-sm text-zinc-700 font-medium">
-            Role
-          </label>
-          <Input.Root>
-            <Input.Control id="role" defaultValue="Product Designer" />
-          </Input.Root>
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="timezone"
+          className="text-sm text-zinc-700 font-medium">
+          Timezone
+        </label>
+        <div />
+      </div>
 
-        <div>
-          <label
-            htmlFor="country"
-            className="text-sm text-zinc-700 font-medium">
-            Country
-          </label>
-          <div />
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="bio"
+          className="text-sm text-zinc-700 font-medium">
+          Bio
+          <span>
+            Write a short introduction.
+          </span>
+        </label>
+        <div />
+      </div>
 
-        <div>
-          <label
-            htmlFor="timezone"
-            className="text-sm text-zinc-700 font-medium">
-            Timezone
-          </label>
-          <div />
-        </div>
+      <div className="pt-5 grid grid-cols-[minmax(7.5rem,_17.5rem)_minmax(25rem,_1fr)_minmax(0,_15rem)]">
+        <label
+          htmlFor="projects"
+          className="text-sm text-zinc-700 font-medium">
+          Portfolio projects
+          <span>
+            Share a few snippets of your work.
+          </span>
+        </label>
+        <div />
+      </div>
 
-        <div >
-          <label
-            htmlFor="bio"
-            className="text-sm text-zinc-700 font-medium">
-            Bio
-            <span>
-              Write a short introduction.
-            </span>
-          </label>
-          <div />
-        </div>
-
-        <div>
-          <label
-            htmlFor="projects"
-            className="text-sm text-zinc-700 font-medium">
-            Portfolio projects
-            <span>
-              Share a few snippets of your work.
-            </span>
-          </label>
-          <div />
-        </div>
-
-        <div className="flex gap-2">
-          <button
-            type="button"
-            className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50 border border-zinc-300"
-          >Cancel</button>
-          <button
-            type="submit"
-            className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-white bg-violet-600 hover:bg-violet-700"
-          >
-            Save
-          </button>
-        </div>
-      </form >
-    </div >
+      <div className="flex gap-2">
+        <button
+          type="button"
+          className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50 border border-zinc-300"
+        >Cancel</button>
+        <button
+          type="submit"
+          className="cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-white bg-violet-600 hover:bg-violet-700"
+        >
+          Save
+        </button>
+      </div>
+    </form>
   )
 }
 
